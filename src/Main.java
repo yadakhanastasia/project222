806a6c2196dedcb2adfd2aa515ab73e3c2fe3efc
yadakhanastasia <yadakh.nastya@gmail.com>
2024-03-12 20:37:45 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+        int c = 67;
+        System.out.println(c/10+c%10);
+
+        double f = 7.5;
+        System.out.println((int)(f+0.5));
+
+    }
+}
